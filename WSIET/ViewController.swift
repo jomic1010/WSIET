91db9bb56f251e13ddd577cd42b3205a6ed6c75e
JomiC <jomic1010@naver.com>
2020-06-30 16:16:31 +0900

    Project Create

---
+++ WSIET/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WSIET
+//
+//  Created by JomiC on 2020/06/30.
+//  Copyright © 2020 JomiC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
