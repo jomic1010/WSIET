d9200cbe308ef1cd125db52194958ab3333787c1
JomiC <jomic1010@naver.com>
2020-07-10 23:31:00 +0900

    fix google map error

---
+++ WSIET/SearchViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SearchViewController.swift
+//  WSIET
+//
+//  Created by JomiC on 2020/07/07.
+//  Copyright © 2020 JomiC. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
